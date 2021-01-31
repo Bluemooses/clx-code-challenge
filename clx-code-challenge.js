ffecc02cc4deca3339c43f5dbcc5c003c67dea46
Jacob Brenke <60531407+Bluemooses@users.noreply.github.com>
2021-01-30 21:11:59 -0600

    Update clx-code-challenge.js

--- clx-code-challenge.js
+++ clx-code-challenge.js
@@ -3,7 +3,7 @@ console.log("You are appreciated.");
 function printCLXChallenge() {
   //initialize for loop on [1 - 100]
   for (let i = 1; i <= 100; i++) {
-    //Multiples of 3 or 5 print CLXDTC
+    //Multiples of 3 and 5 print CLXDTC
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("CLXDTC");
       //Multiples of 5 print DTC
