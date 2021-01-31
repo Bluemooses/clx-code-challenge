5d6db77c55fe1c6d40117cdc7de0e7896fd5c1a6
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-01-30 20:37:59 -0600

    Challenge complete/code commented

--- clx-code-challenge.js
+++ clx-code-challenge.js
@@ -1,1 +1,13 @@
 console.log("You are appreciated.");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("CLXDTC");
+  } else if (i % 5 === 0) {
+    console.log("DTC");
+  } else if (i % 3 === 0) {
+    console.log("CLX");
+  } else {
+    console.log(i);
+  }
+}
