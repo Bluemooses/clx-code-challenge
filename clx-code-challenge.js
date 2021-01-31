d981306bb295b3751de72eb9e7874199db7756d4
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2021-01-30 21:01:18 -0600

    Abstracted for loop to function

--- clx-code-challenge.js
+++ clx-code-challenge.js
@@ -1,18 +1,23 @@
 console.log("You are appreciated.");
 
-//initialize for loop on [1 - 100]
-for (let i = 1; i <= 100; i++) {
+const printCLXChallenge = () => {
+  //initialize for loop on [1 - 100]
+  for (let i = 1; i <= 100; i++) {
     //Multiples of 3 or 5 print CLXDTC
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("CLXDTC");
-    //Multiples of 5 print DTC
-  } else if (i % 5 === 0) {
-    console.log("DTC");
-    //Multiples of 3 print CLX
-  } else if (i % 3 === 0) {
-    console.log("CLX");
-    //Default print i
-  } else {
-    console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("CLXDTC");
+      //Multiples of 5 print DTC
+    } else if (i % 5 === 0) {
+      console.log("DTC");
+      //Multiples of 3 print CLX
+    } else if (i % 3 === 0) {
+      console.log("CLX");
+      //Default print i
+    } else {
+      console.log(i);
+    }
   }
-}
+};
+
+
+printCLXChallenge();
